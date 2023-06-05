95b46541b230b39ac04ffde6ee9db7fd76792f21
Levi Evans <levi.samuel.evans@cern.ch>
2023-06-06 00:14:27 +0100

    Updating condor

--- Condor/reweight.sh
+++ Condor/reweight.sh
@@ -7,4 +7,7 @@ cd /scratch4/levans/data-driven-reweighting/
 source compile.sh
 
 # Run the code
-reweight --configFile configs/config_1l.cfg
+options= 
+options+=$1
+echo "The configs being run on are " ${options}
+reweight --configFile Configs/Nominal/1l/${options}
