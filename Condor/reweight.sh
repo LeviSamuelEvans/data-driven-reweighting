f5b5ff5684086599a516116b22759fed40c811fd
Levi Evans <levi.samuel.evans@cern.ch>
2023-06-05 13:23:47 +0100

    Adding config folder to condor

--- Condor/reweight.sh
+++ Condor/reweight.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Go to reweighting directory 
+# Go to reweighting directory
 cd /scratch4/levans/data-driven-reweighting/
 
 # Compile the code
 source compile.sh
 
 # Run the code
-reweight --configFile config_1l.cfg 
-
-
-
-
-
+reweight --configFile configs/config_1l.cfg
